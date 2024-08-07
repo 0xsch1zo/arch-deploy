3f0475a8e769d132df8a2ce8cde246c2a2ee1c6a
sentientbottleofwine <sentientbottleofwine@gmail.com>
2024-08-07 13:46:43 +0200

    Fix and add colors to nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -5,7 +5,7 @@ MKINITCPIO_MODULES="nvidia nvidia_modeset nvidia_uvm nvidia_drm"
 MODPROBE_CONFIG_DIR="/etc/modprobe.d"
 
 # Add nvidia driver modules to the initramfs
-sudo sed -e "s/^MODULES=(/MODULES(${MKINITCPIO_MODULES}/" /etc/mkinitcpio.conf
+sudo sed -e "s/^MODULES=(/MODULES(${MKINITCPIO_MODULES}/" -i /etc/mkinitcpio.conf
 
 sudo mkdir -p "${MODPROBE_CONFIG_DIR}"
 echo -e "options nvidia_drm modeset=1 fbdev=1\n
@@ -17,7 +17,7 @@ sudo systemctl enable nvidia-suspend.service nvidia-resume.service nvidia-hibern
 # Rebuild mkinitcpio
 sudo mkinitcpio -P
 for time in {10..1}; do
-	echo -ne "\033[2K\r"
+	echo -ne "\033[2K\r\033[31;40m"
 	echo -e "Warning the system will reboot in $time"
 	sleep 1
 done
