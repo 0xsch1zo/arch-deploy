3909a378bfc284b1a6cd451862681fd98967bad0
sentientbottleofwine <sentientbottleofwine@gmail.com>
2024-08-12 10:57:35 +0200

    Fix read and update warning in nvidia.sh

--- src/nvidia.sh
+++ src/nvidia.sh
@@ -8,10 +8,13 @@ MODPROBE_CONFIG_DIR="/etc/modprobe.d"
 MODPROBE_CONFIG_NVIDIA="${MODPROBE_CONFIG_DIR}/nvidia.conf"
 
 echo -ne "\033[2K\r\033[31;40m"
-echo "WARNING: This script doesn't install the kernel headers for you. Please install the apropriate kernel headers for your kernel. You can add the right kernel header package to the packages-bare-bones file"
+echo "WARNING: This script doesn't install the kernel headers for you. If you get 'module not found:' errors that may be why. Please install the apropriate kernel headers for your kernel. You can add the right kernel header package to the packages-bare-bones file"
 echo "Press any key to continue..."
 echo -e "\033[2K\r\033[97;40m"
+
+set +e
 read
+set -e
 
 # Add nvidia driver modules to the initramfs
 modules_found=`grep "${MKINITCPIO_MODULES}" "${MKINITCPIO_CONFIG}"`
@@ -38,4 +41,5 @@ sudo systemctl enable nvidia-suspend.service nvidia-resume.service nvidia-hibern
 sudo mkinitcpio -P
 echo -ne "\033[2K\r\033[31;40m"
 echo -e "Because of setting kernel parameters, and some other stuff a reboot is required for everything to work properly"
+echo -e "\033[2K\r\033[97;40m"
 
